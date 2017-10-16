36555402f47cae71e9805f6fb47e5255997e1039
John Gramila <tynanjk@gmail.com>
2017-10-15 21:27:40 -0500

    added vote result, css ghost buttons

--- src/vote/VoteResult.jsx
+++ src/vote/VoteResult.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function VoteResult ({ vote }) {
 	return (
 		<div class="voteResult">
-			Vote Result
+			<div>
+				<h2>Almost everyone agrees with you.</h2>
+				<h3>100% agree with you.</h3>
+				<div class="voteShare">
+					<h6>1 vote.</h6>
+				</div>
+			</div>
 		</div>
 	);
 }
