41d44f9a64701dccc1599081144300da22014395
John Gramila <tynanjk@gmail.com>
2017-10-19 22:36:44 -0500

    added links out, have to add hover

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="Vote">
-        <OneVoteOneResult/>
+        <OneVoteOneResult />
       </div>
     );
   }
--- src/vote/VoteResult.jsx
+++ src/vote/VoteResult.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import ShareIcons from './ShareIcons.jsx'
-
+import ShareIcon from './ShareIcon.jsx'
+import TwitterLogo from "../static/images/iconmonstr-twitter-4-240.png"
+import FacebookLogo from "../static/images/iconmonstr-facebook-4-240.png"
 
 class VoteResult extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { shareIcons: [{id: 1, image: TwitterLogo, text: "Twitter Icon", value: "twitter"}, 
+		               				{id: 2, image: FacebookLogo, text: "Facebook Icon", value: "facebook"}] };
+	}
+
     render() {
 		return (
-			<div class="voteResult">
-				<div>
-					<h2>Almost everyone agrees with you.</h2>
-					<h3>100% agree with you.</h3>
-					<div class="voteShare">
-						<h6>1 vote.</h6>
-						<h6>Share your opinion.</h6>
+			<div className="voteResult">
+				<h3>Almost everyone agrees with you.</h3>
+				<div className="shareContainer">
+					<div className="shareText">
+						1 vote. <br/>Share your correct opinion.
 					</div>
-					<ShareIcons />
+					<div className="shareIcons">
+						{this.state.shareIcons.map(icon => {
+							return(<ShareIcon key={icon.id}
+							                  shareIcon={icon} />
+							    );
+						})}
+					</div>
 				</div>
 			</div>
 		);
